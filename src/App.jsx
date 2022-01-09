f0a8755c19a926558f55ebfb8a8aa4bd652771a9
ELLIS PAX MAPAKAMA <mapakameellis@gmail.com>
2022-01-09 16:59:25 +0700

    Created new signin method and button- needs modifications tho

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+
+
 import logo from './logo.svg';
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, {Auth} from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import {Button} from '@material-ui/core';
+import {Switch, Route, Link, withRouter, BrowserRouter as Router} from 'react-router-dom';
 
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import SongList from './components/SongList';
 
 
 
 import Storage from "@aws-amplify/storage";
+import SignIn from './components/SignIn';
+import {useHistory} from 'react-router-dom';
 
 
 
@@ -20,20 +25,76 @@ Amplify.configure(awsconfig);
 Storage.configure(awsconfig);
 
 function App() {
-  
+  const [loggedIn, setloggedIn] = useState(false);
+  const history = useHistory();
+
+  useEffect(()=>{
+    AssessLoggedInState()
+  }, [])
+
+  const AssessLoggedInState = ()=>{
+    Auth.currentAuthenticatedUser().then( sess =>{
+      setloggedIn(true);
+    }).catch(()=>{
+      setloggedIn(false);
+     // history.push('/')
+    });
+  };
+
+
+
+
+  const signOut = async()=>{
+    try{
+      await Auth.signOut();      
+      setloggedIn(false);
+      //history.push('/');
+      
+    }catch (error){
+      console.log("Error signing out ", error)
+    }
+
+
+  };
+
+  const onSignIn = ()=>{
+    setloggedIn(true);
+  };
 
   return (
-    <div className="App">      
-      <header className="App-header"> 
-        <AmplifySignOut />
-        <h2>My App Content</h2>
-      </header>
-      <SongList/>
+    <Router>
+      <div className="App">      
+        <header className="App-header"> 
+          { loggedIn ? (
+          <Button variant='contained' color='primary' onClick={signOut}>
+            Sign Out
+          </Button>):(
+          <Link to ='signin'>
+            <Button variant='contained' color='primary'>
+              Sign In
+            </Button>
+          </Link>
+          )}         
+          <h2>Our Music App</h2>
+        </header>
+        <Switch>
+          <Route exact path="/">
+            
+          </Route>
+          <Route path='/home'>
+            <SongList/>
+          </Route>
+          <Route path='/signin'>
+            <SignIn onSignIn={onSignIn}></SignIn>
+          </Route>
+        </Switch>
+        
 
-     </div>
+      </div>
+     </Router>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
 
 
---
+++ src/components/SignIn/index.jsx
@@ -0,0 +1,47 @@
+import React, {useState}from 'react';
+import {Button, TextField} from '@material-ui/core';
+import {useHistory} from 'react-router-dom';
+import {Auth} from 'aws-amplify';
+import '../../App.css';
+
+
+const SignIn = ({onSignIn})=>{
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const signIn = async ()=>{
+        try{
+            const user = await Auth.signIn(username, password);
+            history.push('/home');
+            onSignIn();
+
+
+        }catch(error){
+            console.log('There was na error logging in ', error)
+        }
+    };
+
+    return(
+        <div className='sigin'>
+            <TextField
+                id = 'username'
+                label = 'Username'
+                value = {username}
+                onChange={e=> setUsername(e.target.value)}
+            />
+            <TextField
+                id = 'password'
+                label = 'Password'
+                value = {password}
+                type ='password'
+                onChange={e=> setPassword(e.target.value)}
+            />
+            <Button id='signInButton' color='primary' onClick={signIn}>
+                Submit
+            </Button>
+        </div>
+    )
+}
+
+export default SignIn;
